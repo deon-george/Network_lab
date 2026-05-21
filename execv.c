b14c3fe475e2969e085cabeb472166d197e30f19
deon george <deongeorgevadakkel@gmail.com>
2026-05-21 09:55:44 +0530

    completed execv() program

--- execv.c
+++ execv.c
@@ -1,4 +1,13 @@
-#include<stdio.h>
-#include<unistd.h>
+#include <stdio.h>
+#include <unistd.h>
 
-int main(){
+int main() {
+
+    char *args[] = {"ls", "-l", NULL};
+
+    execv("/bin/ls", args);
+
+    printf("This line will not execute if execv works");
+
+    return 0;
+}
